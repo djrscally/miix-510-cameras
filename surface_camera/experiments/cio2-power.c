514415ac4661f282bd5eda4c8c297b2e1cf077af
Daniel Scally <djrscally@gmail.com>
2020-10-15 14:06:28 +0100

    Improve error handling and exit function

--- surface_camera/experiments/cio2-power.c
+++ surface_camera/experiments/cio2-power.c
@@ -19,6 +19,19 @@
 		.owner		= THIS_MODULE,			\
 	}
 
+struct cio2_power {
+	struct acpi_device *pmic_adev;
+	struct acpi_device *sensor_adev;
+	struct device *pmic;
+	struct device *sensor;
+	struct gpio_desc *avdd_gpiod;
+	struct regulator_dev *avdd;
+	struct gpio_desc *dovdd_gpiod;
+	struct regulator_dev *dovdd;
+};
+
+static struct cio2_power cio2_power;
+
 static int cio2_power_gpio_regulator_is_enabled(struct regulator_dev *rdev)
 {
 	struct gpio_desc *gpio;
@@ -40,7 +53,7 @@ static const struct regulator_desc regulators[] = {
 	CIO2_GPIO_REGULATOR("dovdd", 0, &cio2_power_gpio_regulator_ops),
 };
 
-static const struct gpiod_lookup_table ov2680_gpios = {
+static struct gpiod_lookup_table ov2680_gpios = {
 	.dev_id = "i2c-OVTI2680:00",
 	.table = {
 		GPIO_LOOKUP_IDX("gpiochip0", 120, "xshutdn", 0, GPIO_ACTIVE_LOW),
@@ -50,8 +63,6 @@ static const struct gpiod_lookup_table ov2680_gpios = {
 
 static int cio2_power_register_regulators(struct device *pmic)
 {
-	struct gpio_desc *avdd_gpiod, *dovdd_gpiod;
-	struct regulator_dev *rdev;
 	struct regulator_config avdd_cfg = { };
 	struct regulator_config dovdd_cfg = { };
 	int ret;
@@ -59,34 +70,34 @@ static int cio2_power_register_regulators(struct device *pmic)
 	avdd_cfg.dev = pmic;
 	dovdd_cfg.dev = pmic;
 
-	avdd_gpiod = gpiod_get_index(pmic, NULL, 1, GPIOD_ASIS);
-	if (!avdd_gpiod) {
+	cio2_power.avdd_gpiod = gpiod_get_index(pmic, NULL, 1, GPIOD_ASIS);
+	if (!cio2_power.avdd_gpiod) {
 		pr_err("Error getting gpio 1\n");
 		return -ENODEV;
 	}
 
-	avdd_cfg.ena_gpiod = avdd_gpiod;
-	avdd_cfg.driver_data = avdd_gpiod;
+	avdd_cfg.ena_gpiod = cio2_power.avdd_gpiod;
+	avdd_cfg.driver_data = cio2_power.avdd_gpiod;
 
-	rdev = devm_regulator_register(pmic, &regulators[0], &avdd_cfg);
-	if (!rdev) {
+	cio2_power.avdd = devm_regulator_register(pmic, &regulators[0], &avdd_cfg);
+	if (!cio2_power.avdd) {
 		pr_err("Failed to register AVDD\n");
 		ret = -EINVAL;
 		goto err_free_avdd_gpiod;
 	}
 
-	dovdd_gpiod = gpiod_get_index(pmic, NULL, 2, GPIOD_ASIS);
-	if (!dovdd_gpiod) {
+	cio2_power.dovdd_gpiod = gpiod_get_index(pmic, NULL, 2, GPIOD_ASIS);
+	if (!cio2_power.dovdd_gpiod) {
 		pr_err("Error getting gpio 2\n");
 		ret = -ENODEV;
 		goto err_free_avdd;
 	}
 
-	dovdd_cfg.ena_gpiod = dovdd_gpiod;
-	dovdd_cfg.driver_data = dovdd_gpiod;
+	dovdd_cfg.ena_gpiod = cio2_power.dovdd_gpiod;
+	dovdd_cfg.driver_data = cio2_power.dovdd_gpiod;
 
-	rdev = devm_regulator_register(pmic, &regulators[1], &dovdd_cfg);
-	if (!rdev) {
+	cio2_power.dovdd = devm_regulator_register(pmic, &regulators[1], &dovdd_cfg);
+	if (!cio2_power.dovdd) {
 		pr_err("Failed to register DOVDD\n");
 		ret = -EINVAL;
 		goto err_free_dovdd_gpiod;
@@ -97,67 +108,82 @@ static int cio2_power_register_regulators(struct device *pmic)
 	return 0;
 
 err_free_dovdd_gpiod:
-	gpiod_put(dovdd_gpiod);
+	gpiod_put(cio2_power.dovdd_gpiod);
 err_free_avdd:
-	regulator_unregister(rdev);
+	regulator_unregister(cio2_power.avdd);
 err_free_avdd_gpiod:
-	gpiod_put(avdd_gpiod);
+	gpiod_put(cio2_power.avdd_gpiod);
 	return ret;
-}
-
-static void cio2_power_map_sensor_gpios(void)
-{
-	gpiod_add_lookup_table(&ov2680_gpios);
-
-	pr_info("Mapped xshutdn GPIO successfully\n");
 }
 
 static int cio2_power_init(void)
 {
-	struct acpi_device *sensor_adev;
-	struct acpi_device *pmic_adev;
-	struct device *sensor;
-	struct device *pmic;
 	int ret;
 
-	pmic_adev = acpi_dev_get_first_match_dev("INT3472", "1", -1);
-	if (!pmic_adev) {
+	cio2_power.pmic_adev = acpi_dev_get_first_match_dev("INT3472", "1", -1);
+	if (!cio2_power.pmic_adev) {
 		pr_err("No PMIC acpi_device found\n");
 		return -ENODEV;
 	}
 
-	pmic = bus_find_device_by_acpi_dev(&platform_bus_type, pmic_adev);
-	if (!pmic) {
+	cio2_power.pmic = bus_find_device_by_acpi_dev(&platform_bus_type,
+						      cio2_power.pmic_adev);
+	if (!cio2_power.pmic) {
 		pr_err("No pmic device found\n");
-		return -ENODEV;
+		ret = -ENODEV;
+		goto err_free_pmic_adev;
 	}
 
-	sensor_adev = acpi_dev_get_first_match_dev("OVTI2680", NULL, -1);
-	if (!sensor_adev) {
+	cio2_power.sensor_adev = acpi_dev_get_first_match_dev("OVTI2680", NULL, -1);
+	if (!cio2_power.sensor_adev) {
 		pr_err("No Sensor acpi_device found\n");
-		return -ENODEV;
+		ret = -ENODEV;
+		goto err_free_pmic;
 	}
 
-	sensor = bus_find_device_by_acpi_dev(&i2c_bus_type, sensor_adev);
-	if (!sensor) {
+	cio2_power.sensor = bus_find_device_by_acpi_dev(&i2c_bus_type,
+							cio2_power.sensor_adev);
+	if (!cio2_power.sensor) {
 		pr_err("No sensor dev found\n");
-		return -ENODEV;
+		ret = -ENODEV;
+		goto err_free_sensor_adev;
 	}
 
-	ret = cio2_power_register_regulators(pmic);
+	ret = cio2_power_register_regulators(cio2_power.pmic);
 	if (ret) {
 		pr_err("Failed to register regulators\n");
-		return -EINVAL;
+		ret = -EINVAL;
+		goto err_free_sensor;
 	}
 
-	cio2_power_map_sensor_gpios();
+	gpiod_add_lookup_table(&ov2680_gpios);
 
 	return 0;
+
+err_free_sensor:
+	put_device(cio2_power.sensor);
+err_free_sensor_adev:
+	acpi_dev_put(cio2_power.sensor_adev);
+err_free_pmic:
+	put_device(cio2_power.pmic);
+err_free_pmic_adev:
+	acpi_dev_put(cio2_power.pmic_adev);
+
+	return ret;
 }
 
 static void cio2_power_exit(void)
 {
+	regulator_unregister(cio2_power.avdd);
+	gpiod_put(cio2_power.avdd_gpiod);
 
+	regulator_unregister(cio2_power.dovdd);
+	gpiod_put(cio2_power.dovdd_gpiod);
+
+	put_device(cio2_power.sensor);
+	acpi_dev_put(cio2_power.sensor_adev);
+	put_device(cio2_power.pmic);
+	acpi_dev_put(cio2_power.pmic_adev);
 }
 
 module_init(cio2_power_init);
