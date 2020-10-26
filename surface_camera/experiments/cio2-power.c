4c70679bcc5974795e9c48f7d2190e7fb1e3f25b
Daniel Scally <djrscally@gmail.com>
2020-10-26 10:38:31 +0000

    Validating parsing CRS

--- surface_camera/experiments/cio2-power.c
+++ surface_camera/experiments/cio2-power.c
@@ -1,212 +1,208 @@
 #include <linux/acpi.h>
-#include <linux/device.h>
-#include <linux/gpio/consumer.h>
-#include <linux/gpio/machine.h>
-#include <linux/i2c.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/platform_device.h>
-#include <linux/regulator/driver.h>
-#include <linux/regulator/machine.h>
 
+#define CRS_END_TAG_DESCRIPTOR		0x79
+#define CRS_RESOURCE_SIZE_BITMASK	0x80
+#define CRS_L_RESOURCE_TYPE_BITMASK	0x7f
 
-#define CIO2_GPIO_REGULATOR(_name, _id, _ops)			\
-	{							\
-		.name		= _name,			\
-		.supply_name	= _name,			\
-		.id		= _id,				\
-		.ops		= _ops,				\
-		.type		= REGULATOR_VOLTAGE,		\
-		.owner		= THIS_MODULE,			\
+#define CRS_GPIO_CONN_TYPE_BYTE		0x04
+#define CRS_GPIO_CONSUMER_FLAG_BYTE	0x05
+#define CRS_GPIO_INT_IO_FLAGS_BYTE	0x07
+
+#define CRS_GPIO_PIN_TBL_OFFSET_LOW	0x0e
+#define CRS_GPIO_PIN_TBL_OFFSET_HIGH	0x0f
+
+#define CRS_GPIO_VEND_DATA_OFFSET_LOW	0x13
+#define CRS_GPIO_VEND_DATA_OFFSET_HIGH	0x14
+
+#define CRS_GPIO_RES_NAME_OFFSET_LOW	0x11
+#define CRS_GPIO_RES_NAME_OFFSET_HIGH	0x12
+
+static int cio2_power_parse_gpio(u8 * buffer, u16 len)
+{
+	u16 vendor_data_offset;
+	u16 resource_source_name_offset;
+	u16 resource_source_name_length;
+	u16 i;
+	char *resource_source_name;
+
+	// GPIO Connection Type
+	if (*(buffer + CRS_GPIO_CONN_TYPE_BYTE))
+		pr_info("    GPIO Is an IO Connection\n");
+	else
+		pr_info("    GPIO is an Interrupt Connection\n");
+
+	if (*(buffer + CRS_GPIO_CONSUMER_FLAG_BYTE))
+		pr_info("    Device consumes this GPIO pin\n");
+	else
+		pr_info("    Device produces this GPIO pin\n");
+
+	vendor_data_offset = *(buffer + CRS_GPIO_VEND_DATA_OFFSET_LOW);
+	resource_source_name_offset = *(buffer + CRS_GPIO_RES_NAME_OFFSET_LOW);
+
+	resource_source_name_length = (vendor_data_offset - resource_source_name_offset);
+
+	pr_info("    resource source name length: %d\n", resource_source_name_length);
+
+	resource_source_name = kmalloc(resource_source_name_length, GFP_KERNEL);
+	if (!resource_source_name)
+		return -ENOMEM;
+
+	printk(KERN_INFO "Exact name: ");
+	for (i = 0; i < resource_source_name_length; i++) {
+		printk(KERN_INFO "%c", *(buffer + resource_source_name_offset + i));
+		resource_source_name[i] = *(buffer + resource_source_name_offset + i);
+	}
+	printk(KERN_INFO "\n");
+
+	pr_info("    resource source name: %s\n", resource_source_name);
+	pr_info("    resource source name: %s\n", "\\_SB.PCI0.GPI0");
+
+	acpi_handle handle;
+	acpi_status status;
+
+	char testvar[] = "\\_SB.PCI0.GPI0";
+
+	if (!strcmp(resource_source_name, "\\_SB.PCI0.GPI0"))
+		pr_info("RSN matches manual\n");
+
+	if (!strcmp(testvar, "\\_SB.PCI0.GPI0"))
+		pr_info("testvar matches manual\n");
+
+	if (!strcmp(resource_source_name, testvar))
+		pr_info("Both variables match too\n");
+
+	for (i = 0; i < 15; i++)
+		pr_info("%c %s %c at pos %d\n", resource_source_name[i],
+			resource_source_name[i] == testvar[i] ? "==" : "!=",
+			testvar[i], i);
+
+	pr_info("    resource source name: %s\n", resource_source_name);
+	pr_info("    resource source name: %s\n", testvar);
+
+	status = acpi_get_handle(NULL, resource_source_name, &handle);
+	if (ACPI_FAILURE(status)) {
+		pr_info("    Failed to evaluate Resource Source path. Error Code: %d\n", status);
+	} else {
+		pr_info("    Successfully evaluated Resource Source Path.\n");
+
+		struct acpi_device *gpio_adev;
+		int ret;
+
+		ret = acpi_bus_get_device(handle, &gpio_adev);
+
+		if (!ret)
+			pr_info("    Found ACPI Device: %s\n", dev_name(&gpio_adev->dev));
 	}
 
-struct cio2_power {
-	struct acpi_device *pmic_adev;
-	struct acpi_device *sensor_adev;
-	struct device *pmic;
-	struct device *sensor;
-	struct gpio_desc *avdd_gpiod;
-	struct regulator_dev *avdd;
-	struct gpio_desc *dovdd_gpiod;
-	struct regulator_dev *dovdd;
-};
-
-static struct cio2_power cio2_power;
-
-static int cio2_power_gpio_regulator_is_enabled(struct regulator_dev *rdev)
-{
-	struct gpio_desc *gpio;
-	int value;
-
-	gpio = rdev_get_drvdata(rdev);
-
-	value = gpiod_get_value_cansleep(gpio);
-
-	return value;
+	kfree(resource_source_name);
+	return 0;
+	
 }
 
-static struct regulator_ops cio2_power_gpio_regulator_ops = {
-	.is_enabled         = cio2_power_gpio_regulator_is_enabled,
-};
-
-static const struct regulator_desc regulators[] = {
-	CIO2_GPIO_REGULATOR("avdd", 0, &cio2_power_gpio_regulator_ops),
-	CIO2_GPIO_REGULATOR("dovdd", 0, &cio2_power_gpio_regulator_ops),
-};
-
-static struct gpiod_lookup_table ov2680_gpios = {
-	.dev_id = "i2c-OVTI2680:00",
-	.table = {
-		GPIO_LOOKUP_IDX("INT344B:00", 121, "xshutdn", 0, GPIO_ACTIVE_LOW),
-		{ },
-	},
-};
-
-static struct regulator_init_data avdd_init_data = {
-	.supply_regulator	= NULL,
-	.constraints = {
-		.name		= "avdd",
-		.valid_ops_mask = REGULATOR_CHANGE_STATUS,
-	},
-};
-
-static struct regulator_init_data dovdd_init_data = {
-	.supply_regulator	= NULL,
-	.constraints = {
-		.name		= "dovdd",
-		.valid_ops_mask = REGULATOR_CHANGE_STATUS,
-	},
-};
-
-static int cio2_power_register_regulators(struct device *pmic)
+static int cio2_power_parse_crs(union acpi_object *obj)
 {
-	struct regulator_config avdd_cfg = { };
-	struct regulator_config dovdd_cfg = { };
-	int ret;
+	u8 resource_type = 0x00;
+	u8 tag_byte = 0x00;
+	u16 len;
+	u64 offset = 0;
+	
+	do {
+		tag_byte = *(obj->buffer.pointer + offset);
 
-	avdd_cfg.init_data = &avdd_init_data;
-	dovdd_cfg.init_data = &dovdd_init_data;
+		// get size:
+		if (tag_byte & CRS_RESOURCE_SIZE_BITMASK) {
+			pr_info("Resource at offset 0x%02x is a Large Resource Data Type\n",
+				offset);
 
-	avdd_cfg.dev = pmic;
-	dovdd_cfg.dev = pmic;
+			len = *(obj->buffer.pointer + offset + 1);
 
-	cio2_power.avdd_gpiod = gpiod_get_index(pmic, NULL, 1, GPIOD_ASIS);
-	if (!cio2_power.avdd_gpiod) {
-		pr_err("Error getting gpio 1\n");
-		return -ENODEV;
-	}
+			pr_info("Resource is %d bytes long\n", len);
 
-	avdd_cfg.ena_gpiod = cio2_power.avdd_gpiod;
-	avdd_cfg.driver_data = cio2_power.avdd_gpiod;
+			resource_type = tag_byte & CRS_L_RESOURCE_TYPE_BITMASK;
 
-	cio2_power.avdd = regulator_register(&regulators[0], &avdd_cfg);
-	if (!cio2_power.avdd) {
-		pr_err("Failed to register AVDD\n");
-		ret = -EINVAL;
-		goto err_free_avdd_gpiod;
-	}
+			switch(resource_type) {
+			case 0x00:
+				pr_info("Resource type: reserved\n");
+				break;
+			case 0x01:
+				pr_info("Resource type: 24 bit thing\n");
+				break;
+			case 0x02:
+				pr_info("Resource type: generic Connection Descriptor\n");
+				break;
+			case 0x0c:
+				pr_info("Resource type: GPIO Description\n");
+				cio2_power_parse_gpio(obj->buffer.pointer + offset, len);
+				break;
+			default:
+				pr_info("Fuck only knows\n");
+			}
 
-	cio2_power.dovdd_gpiod = gpiod_get_index(pmic, NULL, 2, GPIOD_ASIS);
-	if (!cio2_power.dovdd_gpiod) {
-		pr_err("Error getting gpio 2\n");
-		ret = -ENODEV;
-		goto err_free_avdd;
-	}
+		}
 
-	dovdd_cfg.ena_gpiod = cio2_power.dovdd_gpiod;
-	dovdd_cfg.driver_data = cio2_power.dovdd_gpiod;
+		offset = offset + len + 0x03;
+		// get type:
 
-	cio2_power.dovdd = regulator_register(&regulators[1], &dovdd_cfg);
-	if (!cio2_power.dovdd) {
-		pr_err("Failed to register DOVDD\n");
-		ret = -EINVAL;
-		goto err_free_dovdd_gpiod;
-	}
-
-	pr_info("Registered regulators successfully\n");
+		// get length:
+	} while (tag_byte != CRS_END_TAG_DESCRIPTOR);
 
 	return 0;
-
-err_free_dovdd_gpiod:
-	gpiod_put(cio2_power.dovdd_gpiod);
-err_free_avdd:
-	regulator_unregister(cio2_power.avdd);
-err_free_avdd_gpiod:
-	gpiod_put(cio2_power.avdd_gpiod);
-	return ret;
 }
 
 static int cio2_power_init(void)
 {
+	struct acpi_buffer buffer = { ACPI_ALLOCATE_BUFFER, NULL };
+	struct acpi_device *adev;
+	union acpi_object *obj;
+	acpi_handle handle;
+	acpi_status status;
 	int ret;
 
-	cio2_power.pmic_adev = acpi_dev_get_first_match_dev("INT3472", "1", -1);
-	if (!cio2_power.pmic_adev) {
+	adev = acpi_dev_get_first_match_dev("INT3472", "0", -1);
+	if (!adev) {
 		pr_err("No PMIC acpi_device found\n");
 		return -ENODEV;
 	}
 
-	cio2_power.pmic = bus_find_device_by_acpi_dev(&platform_bus_type,
-						      cio2_power.pmic_adev);
-	if (!cio2_power.pmic) {
-		pr_err("No pmic device found\n");
+	handle = adev->handle;
+
+	status = acpi_evaluate_object(handle, "_CRS", NULL, &buffer);
+	if (ACPI_FAILURE(status)) {
 		ret = -ENODEV;
 		goto err_free_pmic_adev;
 	}
 
-	cio2_power.sensor_adev = acpi_dev_get_first_match_dev("OVTI2680", NULL, -1);
-	if (!cio2_power.sensor_adev) {
-		pr_err("No Sensor acpi_device found\n");
+	obj = buffer.pointer;
+
+	if (!obj) {
 		ret = -ENODEV;
-		goto err_free_pmic;
+		goto err_free_pmic_adev;
 	}
 
-	cio2_power.sensor = bus_find_device_by_acpi_dev(&i2c_bus_type,
-							cio2_power.sensor_adev);
-	if (!cio2_power.sensor) {
-		pr_err("No sensor dev found\n");
-		ret = -ENODEV;
-		goto err_free_sensor_adev;
-	}
-
-	ret = cio2_power_register_regulators(cio2_power.pmic);
-	if (ret) {
-		pr_err("Failed to register regulators\n");
+	if (obj->type != ACPI_TYPE_BUFFER) {
 		ret = -EINVAL;
-		goto err_free_sensor;
+		goto err_free_pmic_adev;
 	}
 
-	gpiod_add_lookup_table(&ov2680_gpios);
+	pr_info("_CRS buffer length: %d\n", obj->buffer.length);
+	print_hex_dump(KERN_INFO, "", DUMP_PREFIX_OFFSET, 16, 1, obj->buffer.pointer, obj->buffer.length, true);
+
+	ret = cio2_power_parse_crs(obj);
+
+	kfree(buffer.pointer);
+	acpi_dev_put(adev);
 
 	return 0;
 
-err_free_sensor:
-	put_device(cio2_power.sensor);
-err_free_sensor_adev:
-	acpi_dev_put(cio2_power.sensor_adev);
-err_free_pmic:
-	put_device(cio2_power.pmic);
 err_free_pmic_adev:
-	acpi_dev_put(cio2_power.pmic_adev);
+	acpi_dev_put(adev);
 
 	return ret;
 }
 
-static void cio2_power_exit(void)
-{
-	regulator_unregister(cio2_power.avdd);
-	gpiod_put(cio2_power.avdd_gpiod);
-
-	regulator_unregister(cio2_power.dovdd);
-	gpiod_put(cio2_power.dovdd_gpiod);
-
-	gpiod_remove_lookup_table(&ov2680_gpios);
-
-	put_device(cio2_power.sensor);
-	acpi_dev_put(cio2_power.sensor_adev);
-	put_device(cio2_power.pmic);
-	acpi_dev_put(cio2_power.pmic_adev);
-}
+static void cio2_power_exit(void) { }
 
 module_init(cio2_power_init);
 module_exit(cio2_power_exit);
